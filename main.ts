e29a24af139b0df3fe66c241f847ffd2030396af
ER7545 <157691076+ER7545@users.noreply.github.com>
2024-03-06 09:19:54 +0100

    Startfiler for MakeCode-prosjektet

---
+++ main.ts
@@ -0,0 +1,9 @@
+input.onButtonPressed(Button.A, function () {
+    radio.sendString("hsghsdiudbgvurhfsj")
+})
+input.onButtonPressed(Button.B, function () {
+    radio.setGroup(randint(1, 7))
+})
+basic.forever(function () {
+	
+})
